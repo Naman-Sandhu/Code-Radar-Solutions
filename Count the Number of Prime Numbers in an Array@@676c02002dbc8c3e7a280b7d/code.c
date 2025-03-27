6354d3af24c9489abd583a17f8cb984a34a4c7d4
Naman-Sandhu <namansandhu263@gmail.com>
2025-03-28 01:29:34 +0530

    Solution to Count the Number of Prime Numbers in an Array

--- Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
+++ Count the Number of Prime Numbers in an Array@@676c02002dbc8c3e7a280b7d/code.c
@@ -1,34 +1,69 @@
-#include<stdio.h>
+// #include<stdio.h>
+// #include <stdbool.h>
+// bool prime(int x){
+//     int p = 1; 
+//     if (x <= 1) {
+//         return false;
+//     }
+//     for(int i = 2 ; i*i <= x ; i++){
+//         if(x%i==0){
+//             p=0;
+//         }
+//     }
+//     if(p==1){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// int main(){
+//     int a;
+//     scanf("%d",&a);
+//     int n;
+//     scanf("%d",&n);
+//     int array[n];
+//     int size = sizeof(array)/sizeof(array[0]);
+//     for(int i = 0 ; i < size ; i++){
+//         scanf("%d",&array[i]);
+//     }
+//     int count=0;
+//      for(int i = 0 ; i < size ; i++){
+//         if(prime(array[i])){
+//             count++;
+//         }
+//      }  
+//      printf("%d",count);    
+//  }
+
+#include <stdio.h>
 #include <stdbool.h>
-bool prime(int x){
-    int p = 1;
-    for(int i = 2 ; i*i <= x ; i++){
-        if(x%i==0){
-            p=0;
-        }
-    }
-    if(p==1){
-        return true;
-    }
-    else{
+
+bool prime(int x) {
+    if (x <= 1) {
         return false;
     }
-}
-int main(){
-    int a;
-    scanf("%d",&a);
-    int n;
-    scanf("%d",&n);
-    int array[n];
-    int size = sizeof(array)/sizeof(array[0]);
-    for(int i = 0 ; i < size ; i++){
-        scanf("%d",&array[i]);
+    for (int i = 2; i*i <= x; i++) {
+        if (x % i == 0) {
+            return false;
+        }
     }
-    int count=0;
-     for(int i = 0 ; i < size ; i++){
-        if(prime(array[i])){
+    return true;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int array[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (prime(array[i])) {
             count++;
         }
-     }  
-     printf("%d",count);    
- }
+    }
+    printf("%d", count);
+    return 0;
+}
