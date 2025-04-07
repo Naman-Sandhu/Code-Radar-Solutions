30d387e58d5c7c86b8c1ee54902fe69c271ed922
Naman-Sandhu <namansandhu263@gmail.com>
2025-04-07 21:07:05 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,23 +1,32 @@
 #include<stdio.h>
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
-    int arr[n];
-    int size = sizeof(arr)/sizeof(arr[0]);
-    for(int i = 0 ; i < size ; i++){
-        scanf("%d",&arr[i]);
+    scanf("%d", &n);  // Read the size of the array
+    int arr[n];  // Declare the array of size n
+
+    // Input the elements of the array
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    int count = 0 ; 
-    for(int i = 0 ; i < size ; i++){
-        if(arr[i]==arr[i]){
-            count++;
+
+    // Loop through the array and find the frequency of each element
+    for (int i = 0; i < n; i++) {
+        int count = 1;  // Start counting the frequency of arr[i]
+
+        // Check the frequency of arr[i]
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;  // Increment count if we find a match
+                arr[j] = -1;  // Mark arr[j] as counted by setting it to -1
+            }
+        }
+
+        // If arr[i] is not marked as counted, print the frequency
+        if (arr[i] != -1) {
+            printf(" %d %d\n", arr[i], count);
         }
     }
-    int element[count];
-    for(int i = 0 ; i < size ; i++){
-        if(arr[i]=arr[i]){
-            element[count]=arr[i];
-        }
-    }
-    printf("%d",element);
+
+    return 0;
 }
